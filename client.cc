7f4c6afa15639715630d0ed0a6419b2d12175d8a
miboyu <3200102888@zju.edu.cn>
2022-12-02 17:10:05 +0800

    更改了消息队列

--- client.cc
+++ client.cc
@@ -1,7 +1,7 @@
 #include "util.hh"
 class tmp {
 public:
-    long type;
+    __attribute__((unused)) long type;
     char data[BUFSIZE - 2];
 };
 
@@ -14,6 +14,7 @@ private:
     sockaddr_in _server_address{};
     pthread_t connected_thread{};
     int _socket_fd = -1; // socket file descriptor
+    int msgQueueID;
     __attribute__((unused)) static int getChoice() {
         int choice;
         std::cout << "\n\nPlease choose from:\n"
@@ -31,9 +32,16 @@ private:
 
     [[noreturn]] static void *thread_handler(void *input){
         int fileDescriptor = *(int *)input;
-
         char buffer[BUFSIZE];
         recv(fileDescriptor, buffer, BUFSIZE, 0);
+        std::cout << buffer << std::endl;
+        std::cout << "> ";
+        fflush(stdout);
+        key_t key = ftok("/", 'a');
+        int msgQueueID = msgget(key, IPC_CREAT | 0666);
+        if(msgQueueID == -1){
+            std::cout << "[Error] msgget failed, error no is " << errno << std::endl;
+        }
         while(true) {
             memset(buffer, 0, BUFSIZE);
             auto len = recv(fileDescriptor, buffer, BUFSIZE, 0);
@@ -41,13 +49,14 @@ private:
             std::unique_lock<std::mutex> lck(mtx);
             tmp t{};
             if (buffer[0] == FORWORD) {//REPOST
-                std::cout << "You received: " << buffer + 1 << std::endl;
+                std::cout << std::endl << "You received: " << buffer + 1 << std::endl << "> ";
+                fflush(stdout);
+                continue;
             }
+
             t.type = static_cast<unsigned char >(buffer[0]);
             memcpy(t.data, buffer + 1, len - 1);
-            msg_lst.push_back(t);
-
-            cr.notify_all();
+            msgsnd(msgQueueID, &t, BUFSIZE, 0);
         }
     }
 
@@ -64,18 +73,26 @@ private:
     }
 
 public:
-    Client() = default;
+    Client() {
+        _socket_fd = -1;
+        key_t k = ftok("/", 'a');
+        msgQueueID = msgget(k, IPC_CREAT | 0666);
+        char msg[BUFSIZE];
+        while(msgrcv(msgQueueID, &msg, BUFSIZE, 0, IPC_NOWAIT) > 0){
+        }
+    }
     ~Client() {
         if (isConnectionExists()) {
+            char fin = static_cast<char >(DISCONNECT);
+            send(_socket_fd, &fin, sizeof fin, 0);
+            printInfo("connection is closed!");
             close(_socket_fd);
         }
     }
-    void Init() {
-    }
-    [[noreturn]] void run() {
+    void run() {
         while (true) {
             int choice = getChoice();
-            if (choice != 1 && not isConnectionExists()) {
+            if (choice != 1 && choice != 7 && not isConnectionExists()) {
                 printError("NO CONNECTION!");
                 continue;
             }
@@ -91,8 +108,8 @@ public:
 
                     _server_address.sin_family = AF_INET;
                     _server_address.sin_port = htons(port);
-                    _server_address.sin_addr.s_addr = inet_addr(ip.c_str());
-//                    _server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+//                    _server_address.sin_addr.s_addr = inet_addr(ip.c_str());
+                    _server_address.sin_addr.s_addr = htonl(INADDR_ANY);
                     std::cout << _socket_fd << std::endl;
                     if (connect(_socket_fd, (struct sockaddr*)&_server_address, sizeof(struct sockaddr)) == -1) {
                         printError("建立连接失败");
@@ -113,6 +130,7 @@ public:
                         printError("cannot send final signal");
                         break;
                     }
+                    pthread_cancel(connected_thread);
                     close(_socket_fd);
                     _socket_fd = -1;
                     printInfo("disconnect");
@@ -121,37 +139,31 @@ public:
                 case 3: { //get time
                     char get_time = static_cast<char >(PacketType::GET_TIME);
                     send(_socket_fd, &get_time, sizeof get_time, 0);
-                    std::unique_lock<std::mutex> lck(mtx);
-                    while (msg_lst.empty()) {
-                        cr.wait(lck);
+                    tmp t{};
+                    if(msgrcv(msgQueueID, &t, BUFSIZE, PacketType::GET_TIME, 0) < 0){
+                        std::cout << "[Error] msgrcv failed, error no is " << errno << std::endl;
                     }
-                    auto tmp = msg_lst.front();
-                    std::cout << "获取时间：" << tmp.data << std::endl;
-                    msg_lst.pop_front();
+                    std::cout << "获取时间：" << t.data << std::endl;
                     break;
                 }
                 case 4: {
                     char get_name = static_cast<char >(PacketType::GET_NAME);
                     send(_socket_fd, &get_name, sizeof get_name, 0);
-                    std::unique_lock<std::mutex> lck(mtx);
-                    while (msg_lst.empty()) {
-                        cr.wait(lck);
+                    tmp t{};
+                    if(msgrcv(msgQueueID, &t, BUFSIZE, PacketType::GET_NAME, 0) < 0){
+                        std::cout << "[Error] msgrcv failed, error no is " << errno << std::endl;
                     }
-                    auto tmp = msg_lst.front();
-                    std::cout << "获取服务器名称：" << tmp.data << std::endl;
-                    msg_lst.pop_front();
+                    std::cout << "获取服务器名称：" << t.data << std::endl;
                     break;
                 }
                 case 5: {
                     char get_list = static_cast<char >(PacketType::GET_ACTIVE_LIST);
                     send(_socket_fd, &get_list, sizeof get_list, 0);
-                    std::unique_lock<std::mutex> lck(mtx);
-                    while (msg_lst.empty()) {
-                        cr.wait(lck);
+                    tmp t{};
+                    if(msgrcv(msgQueueID, &t, BUFSIZE, PacketType::GET_ACTIVE_LIST, 0) < 0){
+                        std::cout << "[Error] msgrcv failed, error no is " << errno << std::endl;
                     }
-                    auto tmp = msg_lst.front();
-                    std::cout << "获取用户列表：" << tmp.data << std::endl;
-                    msg_lst.pop_front();
+                    std::cout << "获取用户列表：" << t.data << std::endl;
                     break;
                 }
                 case 6: {
@@ -163,24 +175,21 @@ public:
                     printInfo("input port");
                     std::cin >> port;
                     buffer[0] = SEND_MSG;
-                    printInfo("请输入要发送的信息，CTRL-Z结束");
+                    printInfo("请输入要发送的信息，#结束");
                     sprintf(buffer + 1, "%s:%d:\n", ip.c_str(), port);
-                    char c;
-                    while(c!='#') {
+                    char c = ' ';
+                    while(c != '#') {
                         std::cin >> c;
                         sprintf(buffer + strlen(buffer), "%c", c);
                     }
-
                     std::cout << buffer << std::endl;
-
                     send(_socket_fd, &buffer, sizeof buffer, 0);
-                    std::unique_lock<std::mutex> lck(mtx);
-                    while (msg_lst.empty()) {
-                        cr.wait(lck);
+                    std::cout << "发送信息：" << std::endl;
+                    tmp t{};
+                    if(msgrcv(msgQueueID, &t, BUFSIZE, PacketType::SEND_MSG, 0) < 0){
+                        std::cout << "[Error] msgrcv failed, error no is " << errno << std::endl;
                     }
-                    auto tmp = msg_lst.front();
-                    std::cout << "发送信息：" << tmp.data << std::endl;
-                    msg_lst.pop_front();
+                    std::cout << "[Server] " << t.data << std::endl;
                     break;
                 }
                 case 7: {
@@ -189,7 +198,7 @@ public:
                         send(_socket_fd, &fin, sizeof fin, 0);
                         close(_socket_fd);
                     }
-                    exit(0);
+                    return;
                 }
                 default:{
                     std::cout << "不合法的选项。请重新输入" << std::endl;
@@ -200,12 +209,7 @@ public:
 };
 
 int main() {
-    Client client;
-    try {
-        client.Init();
-    } catch (std::exception &e) {
-        std::cerr << "cannot build a socket connection" << std::endl;
-        return 0;
-    }
-    client.run();
+    auto* client = new Client();
+    client->run();
+    delete client;
 }
